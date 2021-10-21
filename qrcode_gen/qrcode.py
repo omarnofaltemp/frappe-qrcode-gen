edd3c6eb6cc32bb606c88c742a100659efd76b07
Omar Ghouka <omarghouka@gmail.com>
2021-10-21 17:33:47 +0300

    custom qr code

--- qrcode_gen/qrcode.py
+++ qrcode_gen/qrcode.py
@@ -1,11 +1,12 @@
 import frappe
 import pyqrcode
-
-
-
+from frappe.utils import get_url
 
 @frappe.whitelist()
-def gen_qrcode(text):
-    data = pyqrcode.create(text)
+def gen_qrcode(doctype,name,print_format):
+    key = str(frappe.get_doc(doctype, name).get_signature())
+    url = str(get_url()) + "/" + doctype + "/" + str(name) + "?format=" + str(print_format) + "&key=" + key
 
+    data = pyqrcode.create(url)
+    
     return f'data:image/png;base64,{data.png_as_base64_str(scale=5)}'
