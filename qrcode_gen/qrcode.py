7d5ff51550998cea3752cc565839cc68c92a405a
omandan <o.m201118a.n@gmial.com>
2021-10-22 19:11:57 +0300

    fix hook and url sentax and function name qrcod.py

--- qrcode_gen/qrcode.py
+++ qrcode_gen/qrcode.py
@@ -3,10 +3,14 @@ import pyqrcode
 from frappe.utils import get_url
 
 @frappe.whitelist()
-def gen_qrcode(doctype,name,print_format):
+def printformat_url_qr(doctype,name,print_format):
     key = str(frappe.get_doc(doctype, name).get_signature())
     url = str(get_url()) + "/" + doctype + "/" + str(name) + "?format=" + str(print_format) + "&key=" + key
 
-    data = pyqrcode.create(url)
+    data = pyqrcode.create(url.replace(" ","%20"))
     
     return f'data:image/png;base64,{data.png_as_base64_str(scale=5)}'
+
+@frappe.whitelist()
+def gen_qrcode(text):
+    return f'data:image/png;base64,{pyqrcode.create(text).png_as_base64_str(scale=5)}'
